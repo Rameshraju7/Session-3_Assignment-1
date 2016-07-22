85db5dabade80d2fe3b1f3483bee4424a3eb39b1
Rameshraju7 <mallelaramesh5@gmail.com>
2016-07-22 13:59:23 +0530

    Add files via upload

---
+++ sqrs_cbrt.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Ramesh
+ *
+ */
+public class sqrs_cbrt {
+
+	static int marksOne;
+   
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner in = new Scanner(System.in);
+			
+			System.out.println("Enter the value = ");
+			marksOne= in.nextInt();
+			
+			System.out.println("Enter the value = "+marksOne);	
+	
+		System.out.println("Squar root of given number = "+Math.sqrt(marksOne));
+		System.out.println("cube rootof given number = "+Math.cbrt(marksOne));
+		
+		
+		
+
+	}
+
+}
